903d1b018f5318b9aca9446bf63bbb4e73688cd9
heewonk <heewonk@netflix.com>
2020-02-27 12:33:36 -0800

    adding react version of ui

---
+++ consoleme-ui/src/static/js/audit.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+  // Search on each header
+  $('#audit thead th').each(function () {
+    let title = $('#audit thead th').eq($(this).index()).text();
+    if (title == "Updated At") {
+      $(this).html(
+        '<input class="date_range_filter date" type="text" id="datepicker_from" placeholder="From Date (01/01/2019)"/>' +
+        '<input class="date_range_filter date" type="text" id="datepicker_to" placeholder="To Date (12/31/2019)"/>');
+
+    } else {
+      $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    }
+  });
+
+  let table = $('#audit').DataTable({
+    "order": [],
+    "dom": 'Bfrtip',
+    "buttons": ['csvHtml5'],
+    "initComplete": function () {
+      $('#hide').css('display', 'block');
+      $('#loading').css('display', 'none');
+    },
+    "lengthMenu": [[10, 25, 50, 100, 1000], [10, 25, 50, 100, 1000]],
+    "displayLength": 25,
+    "processing": true,
+    "serverSide": true,
+    "ajax": {
+      "url": "get_audit_logs",
+      "data": function (d) {
+        return $.extend({}, d, {
+          "datepicker_from": $('#datepicker_from').val(),
+          "datepicker_to": $('#datepicker_to').val()
+        });
+      }
+    },
+    "search": {
+      "regex": true
+    },
+    "columns": [
+      {
+        "orderable": false,
+        "render": function (data, type, row, meta) {
+          if (type === 'display') {
+            data = '<a target="_blank" href="/accessui/group/' + data + '">' + data + '</a>';
+          }
+
+          return data;
+        }
+      },
+      {"orderable": false},
+      {
+        "orderable": false,
+        "render": function (data, type, row, meta) {
+          if (type === 'display') {
+            data = '<a target="_blank" href="/accessui/user/' + data + '">' + data + '</a>';
+          }
+
+          return data;
+        }
+      },
+      {
+        "orderable": false,
+        "render": function (data, type, row, meta) {
+          if (type === 'display') {
+            data = '<a target="_blank" href="/accessui/user/' + data + '">' + data + '</a>';
+          }
+
+          return data;
+        }
+      },
+      {"orderable": false},
+      {"orderable": false},
+    ],
+    "sDom": '<"top"i>rt<"bottom"lpB><"clear">'
+  });
+
+  let input_filter_timeout;
+
+// Apply the search
+  table.columns().eq(0).each(function (colIdx) {
+    $('input', table.column(colIdx).header()).on('keyup search input paste cut keydown', function (ev) {
+      that = this;
+      clearTimeout(input_filter_timeout);
+      input_filter_timeout = setTimeout(function () {
+          table
+            .column(colIdx)
+            .search(that.value)
+            .draw();
+        },
+        350);
+    });
+  });
+
+// Allow searching on the dropdown
+  table.columns().eq(0).each(function (colIdx) {
+    $('select', table.column(colIdx).header()).on('keyup change', function () {
+      clearTimeOut()
+      table
+        .column(colIdx)
+        .search(this.value)
+        .draw();
+    });
+  });
+});
---
+++ consoleme-ui/src/static/js/group_footer.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  $('#members').DataTable({
+    'order': [[1, 'asc']],
+    'displayLength': 25
+  })
+})
+
+$('.message .close').on('click', function () {
+  $(this).closest('.message').transition('fade')
+})
+
+document.getElementById('group_modify_form').addEventListener('submit', async function (event) {
+  event.preventDefault()
+
+  document.getElementById('error_div').classList.add('hidden')
+  document.getElementById('success_div').classList.add('hidden')
+
+  let arr = $(this).serializeArray()
+  let json = JSON.stringify(arr)
+  $('.ui.dimmer').addClass('active')
+  let res = await sendRequestCommon(json)
+  $('.ui.dimmer').removeClass('active')
+  let element = null
+
+  if (res.status === 'error') {
+    element = document.getElementById('error_response')
+    element.textContent = res.message
+    document.getElementById('error_div').classList.remove('hidden')
+  } else if (res.status === 'success') {
+    element = document.getElementById('success_response')
+    element.textContent = res.message
+    document.getElementById('success_div').classList.remove('hidden')
+  } else {
+    element = document.getElementById('error_response')
+    element.textContent = event.target.responseText
+    document.getElementById('error_div').classList.remove('hidden')
+  }
+
+  element = document.getElementById('modal_content')
+  element.innerHTML = res.html
+  $('.ui.basic.modal')
+    .modal('show')
+}
+)
