cb5516a92348c18bedd93103c3010a60051b0095
Curtis Castrapel <ccastrapel@netflix.com>
2020-05-05 12:02:31 -0700

    Minor exception changes

--- consoleme/exceptions/exceptions.py
+++ consoleme/exceptions/exceptions.py
@@ -279,10 +279,14 @@ class KriegerError(BaseException):
         super().__init__(msg)
 
 
-class BaseWebpackLoaderException(Exception):
+class BaseWebpackLoaderException(BaseException):
     """
     Base exception for django-webpack-loader.
     """
+
+    def __init__(self, msg=""):
+        stats.count("BaseWebpackLoaderException")
+        super().__init__(msg)
 
 
 class WebpackError(BaseWebpackLoaderException):
@@ -290,11 +294,19 @@ class WebpackError(BaseWebpackLoaderException):
     General webpack loader error.
     """
 
+    def __init__(self, msg=""):
+        stats.count("WebpackLoaderBadStatsError")
+        super().__init__(msg)
+
 
 class WebpackLoaderBadStatsError(BaseWebpackLoaderException):
     """
     The stats file does not contain valid data.
     """
+
+    def __init__(self, msg=""):
+        stats.count("WebpackLoaderBadStatsError")
+        super().__init__(msg)
 
 
 class WebpackLoaderTimeoutError(BaseWebpackLoaderException):
@@ -302,11 +314,19 @@ class WebpackLoaderTimeoutError(BaseWebpackLoaderException):
     The bundle took too long to compile.
     """
 
+    def __init__(self, msg=""):
+        stats.count("WebpackLoaderTimeoutError")
+        super().__init__(msg)
+
 
 class WebpackBundleLookupError(BaseWebpackLoaderException):
     """
     The bundle name was invalid.
     """
+
+    def __init__(self, msg=""):
+        stats.count("WebpackBundleLookupError")
+        super().__init__(msg)
 
 
 class UnsupportedRedisDataType(BaseException):
