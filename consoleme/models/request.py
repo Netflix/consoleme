fa6c92bdb4e29fa8626020671ebf26729deab4f7
Patrick Sanders <psanders@netflix.com>
2020-05-14 12:47:49 -0700

    rename models file, add request subclasses

--- consoleme/models/request.py
+++ consoleme/models/request.py
@@ -1,11 +1,11 @@
-from consoleme.models.models import ExtendedRequestModel
-from consoleme.models.models import RequestModel
+from consoleme.models.base_models import ExtendedRequestModel
+from consoleme.models.base_models import RequestModel
 
 
 class Request(ExtendedRequestModel):
     def __init__(self, *args, **kwargs):
         super().__init__(self)
 
-    def store(self):
+    def store(self, dynamodb_client):
         """save to dynamodb"""
         pass
