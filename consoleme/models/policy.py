47cd4740679cf1bfdc7dcf2a0661279134ca9b64
Patrick Sanders <psanders@netflix.com>
2020-05-20 16:19:23 -0700

    Remove model subclasses and move models.py up a level

--- consoleme/models/policy.py
+++
@@ -1,6 +0,0 @@
-from consoleme.models.base_models import PolicyModel
-
-
-class Policy(PolicyModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(self)
--- consoleme/models/role.py
+++
@@ -1,7 +0,0 @@
-from consoleme.models.base_models import ExtendedRoleModel
-from consoleme.models.base_models import RoleModel
-
-
-class Role(ExtendedRoleModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(self)
