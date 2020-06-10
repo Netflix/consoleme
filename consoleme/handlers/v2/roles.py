a0331e4aecf3885643d705552a29cfa531704c5d
Curtis Castrapel <ccastrapel@netflix.com>
2020-06-10 12:52:49 -0700

    WIP - Extended Role Details

--- consoleme/handlers/v2/roles.py
+++ consoleme/handlers/v2/roles.py
@@ -4,11 +4,14 @@ from consoleme.config import config
 from consoleme.handlers.base import BaseAPIV2Handler
 from consoleme.lib.crypto import Crypto
 from consoleme.lib.plugins import get_plugin_by_name
+from consoleme.lib.v2.roles import get_role_details
 
 stats = get_plugin_by_name(config.get("plugins.metrics"))()
 log = config.get_logger()
 crypto = Crypto()
 auth = get_plugin_by_name(config.get("plugins.auth"))()
+aws = get_plugin_by_name(config.get("plugins.aws"))()
+internal_policies = get_plugin_by_name(config.get("plugins.internal_policies"))()
 
 
 class RolesHandler(BaseAPIV2Handler):
@@ -66,19 +69,35 @@ class RoleDetailHandler(BaseAPIV2Handler):
 
     allowed_methods = ["GET", "PUT"]
 
-    async def get(self, account_number, role_name):
+    async def get(self, account_id, role_name):
         """
         GET /api/v2/roles/{account_number}/{role_name}
         """
         log_data = {
             "function": "RoleDetailHandler.get",
             "user": self.user,
-            "message": "Writing all eligible user roles",
+            "ip": self.ip,
+            "message": "Retrieving role details",
             "user-agent": self.request.headers.get("User-Agent"),
             "request_id": self.request_uuid,
+            "account_id": account_id,
+            "role_name": role_name,
         }
+        stats.count(
+            "RoleDetailHandler.get",
+            tags={"user": self.user, "account_id": account_id, "role_name": role_name},
+        )
         log.debug(log_data)
-        self.write_error(501, message="Get role details")
+        role_details = await get_role_details(account_id, role_name)
+
+        if not role_details:
+            self.send_error(
+                404,
+                message=f"Unable to retrieve the specified role: {account_id}/{role_name}",
+            )
+            return
+
+        self.write(role_details.dict())
 
     async def put(self, account_id, role_name):
         """
