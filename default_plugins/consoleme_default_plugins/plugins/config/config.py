0759fe537c7ccec30c340695bf5c4e8f96f80602
Curtis Castrapel <ccastrapel@gmail.com>
2020-06-22 21:31:57 -0700

    Refactor docker directory as example_config, as it is more intuitive

--- default_plugins/consoleme_default_plugins/plugins/config/config.py
+++ default_plugins/consoleme_default_plugins/plugins/config/config.py
@@ -6,7 +6,7 @@ class Config:
     def get_config_location():
         if os.environ.get("CONFIG_LOCATION"):
             return os.environ.get("CONFIG_LOCATION")
-        path = "docker/example_config_test.yaml"
+        path = "example_config/example_config_test.yaml"
         return path
 
     @staticmethod
