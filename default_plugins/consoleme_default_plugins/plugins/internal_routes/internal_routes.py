f1db3e6637c8580824e4da165ff9351099dfe8e1
Curtis Castrapel <ccastrapel@netflix.com>
2020-06-24 10:18:51 -0700

    Fix path reference

--- default_plugins/consoleme_default_plugins/plugins/internal_routes/internal_routes.py
+++ default_plugins/consoleme_default_plugins/plugins/internal_routes/internal_routes.py
@@ -1,25 +1,24 @@
-import os
-
-import pkg_resources
 from consoleme_default_plugins.plugins.internal_routes.handlers.internal_demo_route import (
     InternalDemoRouteHandler,
 )
-
-from consoleme.handlers.base import NoCacheStaticFileHandler
 
 
 class InternalRoutes:
     ui_modules = {}
 
     def get_internal_routes(self, make_jwt_validator, jwt_validator=None):
-        path = pkg_resources.resource_filename("consoleme_internal", "templates")
+        # The below code can be used with your ConsoleMe Internal package name to generate a path to your internal
+        # JavaScript and HTML files, if you wish to render these for the handler.
+        # path = pkg_resources.resource_filename("consoleme_internal", "templates")
         internal_routes = [
             (r"/internal_demo_route/?", InternalDemoRouteHandler),
-            (
-                r"/static_internal/(.*)",
-                NoCacheStaticFileHandler,
-                dict(path=os.path.join(path, "static")),
-            ),
+            # An example of serving static internal content is below, which would make use of the template path variable
+            # You defined above.
+            # (
+            #     r"/static_internal/(.*)",
+            #     NoCacheStaticFileHandler,
+            #     dict(path=os.path.join(path, "static")),
+            # ),
         ]
         return internal_routes
 
