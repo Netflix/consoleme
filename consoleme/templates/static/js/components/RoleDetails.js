a646c7d90b33d6b18495982567fd040e17d1539b
Curtis Castrapel <ccastrapel@netflix.com>
2020-08-06 14:38:08 -0700

    ESLint AutoFix Components

--- consoleme/templates/static/js/components/RoleDetails.js
+++ consoleme/templates/static/js/components/RoleDetails.js
@@ -1,169 +1,165 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
-    Header,
-    Icon,
-    List,
-    Segment,
+  Header,
+  Icon,
+  List,
+  Segment,
 } from 'semantic-ui-react';
 
-
 function RoleDetails(props) {
-    const role = props.role;
-    if (!role) {
-        return (
-            <Segment placeholder>
-                <Header icon>
-                    <Icon name="braille" />
-                    No Role is selected for display.
-                </Header>
-            </Segment>
-        );
-    }
-
+  const role = props.role;
+  if (!role) {
     return (
-        <List divided relaxed="very">
-            <List.Item>
-                <List.Icon name='key' />
-                <List.Content>
-                    <List.Header>Account</List.Header>
-                    <List.Description>
-                        {`${role.account_name} (${role.account_id})`}
-                    </List.Description>
-                </List.Content>
-            </List.Item>
-            <List.Item>
-                <List.Icon name='code' />
-                <List.Content>
-                    <List.Header>Application</List.Header>
-                    <List.List>
-                        {
-                            role.apps.app_details.map((app) => {
-                                return (
-                                    <List.Item>
-                                        <List.Icon name="file code" />
-                                        <List.Content>
-                                            <List.Header>
-                                                <a target="_blank" href={app.app_url}>
-                                                    {app.name}
-                                                </a>
-                                            </List.Header>
-                                            <List.Description>
-                                                {app.owner}
-                                            </List.Description>
-                                        </List.Content>
-                                    </List.Item>
-                                );
-                            })
-                        }
-                    </List.List>
-                </List.Content>
-            </List.Item>
-            <List.Item>
-                <List.Icon name='edit' />
-                <List.Content>
-                    <List.Header>Role</List.Header>
-                    <List.List>
-                        <List.Item>
-                            <List.Icon name="file" />
-                            <List.Content>
-                                <List.Header>
-                                    NAME
-                                </List.Header>
-                                <List.Description>
-                                    {role.name}
-                                </List.Description>
-                            </List.Content>
-                        </List.Item>
-                        <List.Item>
-                            <List.Icon name="file alternate" />
-                            <List.Content>
-                                <List.Header>
-                                    ARN
-                                </List.Header>
-                                <List.Description>
-                                    <a
-                                        target="_blank"
-                                        href={`/policies/edit/${role.account_id}/iamrole/${role.name}`}>
-                                        {role.arn}
-                                    </a>
-                                </List.Description>
-                            </List.Content>
-                        </List.Item>
-                        <List.Item>
-                            <List.Icon name="tags" />
-                            <List.Content>
-                                <List.Header>
-                                    TAGS
-                                </List.Header>
-                                <List.List>
-                                    {
-                                        role.tags.map((tag) => {
-                                            return (
-                                                <List.Item>
-                                                    <List.Icon name="tag" />
-                                                    <List.Content>
-                                                        <List.Header>
-                                                            {tag.Key}
-                                                        </List.Header>
-                                                        <List.Description>
-                                                            {tag.Value}
-                                                        </List.Description>
-                                                    </List.Content>
-                                                </List.Item>
-                                            )
-                                        })
-                                    }
-                                </List.List>
-                            </List.Content>
-                        </List.Item>
-                        <List.Item>
-                            <List.Icon name='clone' />
-                            <List.Content>
-                                <List.Header>Template</List.Header>
-                                <List.Description>
-                                    {
-                                        role.templated
-                                            ? "This is role is templated by Honeybee"
-                                            : "There is no template for this role"
-                                    }
-                                </List.Description>
-                            </List.Content>
-                        </List.Item>
-                    </List.List>
-                </List.Content>
-            </List.Item>
-            <List.Item>
-                <List.Icon name='history' />
-                <List.Content>
-                    <List.Header>Activity</List.Header>
-                    <List.List>
-                        <List.Item>
-                            <List.Icon name="cloud" />
-                            <List.Content>
-                                <List.Header>
-                                    Cloud Trails
-                                </List.Header>
-                                <List.Description>
-                                    There are <a href={role.cloudtrail_details.error_url} target="_blank">{role.cloudtrail_details.errors.cloudtrail_errors.length}</a> errors.
-                                </List.Description>
-                            </List.Content>
-                        </List.Item>
-                        <List.Item>
-                            <List.Icon name="bitbucket" />
-                            <List.Content>
-                                <List.Header>
-                                    S3 Access Logs
-                                </List.Header>
-                                <List.Description>
-                                    There are <a href={role.s3_details.error_url} target="_blank">{role.s3_details.errors.s3_errors.length}</a> errors.
-                                </List.Description>
-                            </List.Content>
-                        </List.Item>
-                    </List.List>
-                </List.Content>
-            </List.Item>
-        </List>
+      <Segment placeholder>
+        <Header icon>
+          <Icon name="braille" />
+          No Role is selected for display.
+        </Header>
+      </Segment>
     );
+  }
+
+  return (
+    <List divided relaxed="very">
+      <List.Item>
+        <List.Icon name='key' />
+        <List.Content>
+          <List.Header>Account</List.Header>
+          <List.Description>
+            {`${role.account_name} (${role.account_id})`}
+          </List.Description>
+        </List.Content>
+      </List.Item>
+      <List.Item>
+        <List.Icon name='code' />
+        <List.Content>
+          <List.Header>Application</List.Header>
+          <List.List>
+            {
+                            role.apps.app_details.map((app) => (
+                              <List.Item>
+                                <List.Icon name="file code" />
+                                <List.Content>
+                                  <List.Header>
+                                    <a target="_blank" href={app.app_url}>
+                                      {app.name}
+                                    </a>
+                                  </List.Header>
+                                  <List.Description>
+                                    {app.owner}
+                                  </List.Description>
+                                </List.Content>
+                              </List.Item>
+                            ))
+                        }
+          </List.List>
+        </List.Content>
+      </List.Item>
+      <List.Item>
+        <List.Icon name='edit' />
+        <List.Content>
+          <List.Header>Role</List.Header>
+          <List.List>
+            <List.Item>
+              <List.Icon name="file" />
+              <List.Content>
+                <List.Header>
+                  NAME
+                </List.Header>
+                <List.Description>
+                  {role.name}
+                </List.Description>
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="file alternate" />
+              <List.Content>
+                <List.Header>
+                  ARN
+                </List.Header>
+                <List.Description>
+                  <a
+                    target="_blank"
+                    href={`/policies/edit/${role.account_id}/iamrole/${role.name}`}
+                  >
+                    {role.arn}
+                  </a>
+                </List.Description>
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="tags" />
+              <List.Content>
+                <List.Header>
+                  TAGS
+                </List.Header>
+                <List.List>
+                  {
+                                        role.tags.map((tag) => (
+                                          <List.Item>
+                                            <List.Icon name="tag" />
+                                            <List.Content>
+                                              <List.Header>
+                                                {tag.Key}
+                                              </List.Header>
+                                              <List.Description>
+                                                {tag.Value}
+                                              </List.Description>
+                                            </List.Content>
+                                          </List.Item>
+                                        ))
+                                    }
+                </List.List>
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name='clone' />
+              <List.Content>
+                <List.Header>Template</List.Header>
+                <List.Description>
+                  {
+                                        role.templated
+                                          ? 'This is role is templated by Honeybee'
+                                          : 'There is no template for this role'
+                                    }
+                </List.Description>
+              </List.Content>
+            </List.Item>
+          </List.List>
+        </List.Content>
+      </List.Item>
+      <List.Item>
+        <List.Icon name='history' />
+        <List.Content>
+          <List.Header>Activity</List.Header>
+          <List.List>
+            <List.Item>
+              <List.Icon name="cloud" />
+              <List.Content>
+                <List.Header>
+                  Cloud Trails
+                </List.Header>
+                <List.Description>
+                  There are <a href={role.cloudtrail_details.error_url} target="_blank">{role.cloudtrail_details.errors.cloudtrail_errors.length}</a> errors.
+                </List.Description>
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="bitbucket" />
+              <List.Content>
+                <List.Header>
+                  S3 Access Logs
+                </List.Header>
+                <List.Description>
+                  There are <a href={role.s3_details.error_url} target="_blank">{role.s3_details.errors.s3_errors.length}</a> errors.
+                </List.Description>
+              </List.Content>
+            </List.Item>
+          </List.List>
+        </List.Content>
+      </List.Item>
+    </List>
+  );
 }
 
 export default RoleDetails;
