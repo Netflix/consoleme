fa58cf2dcd88f2328d66b95116896c421114c1d2
Kinnaird McQuade <kmcquade@salesforce.com>
2020-08-10 18:22:02 -0400

    Removed the cache_aws_account_information celery task that was missing and added the [0] to the readme output in terraform

--- terraform/central-account/outputs.tf
+++ terraform/central-account/outputs.tf
@@ -13,7 +13,7 @@ If you're following the Terraform demo environment and your Terraform apply was
 successful, you can wait for a few minutes and then visit the ConsoleMe dashboard
 at the following URL:
 
-$ http://${module.server.public_ip}:8081
+$ http://${module.server.public_ip[0]}:8081
 
 If you want to access the server, make sure you add your private key to your
 SSH agent:
@@ -22,7 +22,7 @@ $ ssh-add path/to/private_key.pem
 
 Then SSH into the server:
 
-$ ssh -A ec2-user@${module.server.public_ip}
+$ ssh -A ec2-user@${module.server.public_ip[0]}
 
 If you want to experiment with the ConsoleMe config, you can find the config files at the path:
 
