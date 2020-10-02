df7fae5788e133c595e81b4b9faa375131553ec4
Curtis Castrapel <ccastrapel@netflix.com>
2020-10-02 09:13:43 -0700

    Remove groups dropdown; add protected routes for policies and requests; remove ncu ; selfservice enabled by default

--- ui/src/App.js
+++ ui/src/App.js
@@ -13,38 +13,52 @@ const LOCAL_KEY = "consoleMeLocalStorage";
 
 // TODO(heewonk), come up with better 404 page
 const NoMatch = ({ location }) => (
-  <h3>No match for <code>{location.pathname}</code></h3>
+  <h3>
+    No match for <code>{location.pathname}</code>
+  </h3>
 );
 
 function App() {
-    return (
-        <BrowserRouter>
-            <AuthProvider>
-                <Switch>
-                    <ProtectedRoute
-                        exact
-                        path="/ui/"
-                        component={ConsoleMeDataTable}
-                        configEndpoint={"/api/v2/role_table_config"}
-                        queryString={""}
-                    />
-                    <ProtectedRoute
-                        exact
-                        path="/ui/selfservice"
-                        component={ConsoleMeSelfService}
-                    />
-                    <ProtectedRoute
-                        exact
-                        path="/ui/catalog"
-                        component={ConsoleMeCatalog}
-                    />
-                    <Route
-                        component={NoMatch}
-                    />
-                </Switch>
-            </AuthProvider>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Switch>
+          <ProtectedRoute
+            exact
+            path="/ui/"
+            component={ConsoleMeDataTable}
+            configEndpoint={"/api/v2/role_table_config"}
+            queryString={""}
+          />
+          <ProtectedRoute
+            exact
+            path="/ui/selfservice"
+            component={ConsoleMeSelfService}
+          />
+          <ProtectedRoute
+            exact
+            path="/ui/policies"
+            component={ConsoleMeDataTable}
+            configEndpoint={"/api/v2/policies_table_config"}
+            queryString={""}
+          />
+          <ProtectedRoute
+            exact
+            path="/ui/requests"
+            component={ConsoleMeDataTable}
+            configEndpoint={"/api/v2/requests_table_config"}
+            queryString={""}
+          />
+          <ProtectedRoute
+            exact
+            path="/ui/catalog"
+            component={ConsoleMeCatalog}
+          />
+          <Route component={NoMatch} />
+        </Switch>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -1,170 +1,110 @@
-import React, { useEffect } from 'react';
-import {
-    Dropdown,
-    Menu,
-    Image,
-} from 'semantic-ui-react';
-import {NavLink} from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Dropdown, Menu, Image } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
 import { useAuth } from "../auth/AuthContext";
 
-
 const ConsoleMeHeader = () => {
-    const { authState } = useAuth();
-    const { userInfo } = authState;
+  const { authState } = useAuth();
+  const { userInfo } = authState;
 
-    if (!authState.isAuthenticated || !userInfo) {
-        return null;
+  if (!authState.isAuthenticated || !userInfo) {
+    return null;
+  }
+
+  const generatePoliciesDropDown = () => {
+    if (userInfo.pages.policies.enabled === true) {
+      return (
+        <Dropdown text="Roles and Policies" pointing className="link item">
+          <Dropdown.Menu>
+            <Dropdown.Item as={NavLink} to="/ui/catalog">
+              Service Catalog
+            </Dropdown.Item>
+            <Dropdown.Item as={NavLink} to="/ui/policies">
+              Policies
+            </Dropdown.Item>
+            <Dropdown.Item as={NavLink} to="/ui/selfservice">
+              Self Service Permissions
+            </Dropdown.Item>
+            <Dropdown.Item as={NavLink} to="/ui/requests">
+              All Policy Requests
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      );
     }
+    return null;
+  };
 
-    const generateGroupsDropDown = () => {
-        if (userInfo.pages.groups.enabled === true) {
-            return (
-                <Dropdown
-                    text='Group Access'
-                    pointing
-                    className='link item'
-                >
-                    <Dropdown.Menu>
-                        <Dropdown.Item>
-                            Request Access
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                            Groups
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                            Users
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                            Pending
-                        </Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            );
-        }
-        return null;
+  const generateAdvancedDropDown = () => {
+    if (userInfo.pages.config.enabled === true) {
+      return (
+        <Dropdown text="Advanced" pointing className="link item">
+          <Dropdown.Menu>
+            <Dropdown.Item as={NavLink} to="/ui/config">
+              Config
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      );
     }
+    return null;
+  };
 
-    const generatePoliciesDropDown = () => {
-        if (userInfo.pages.policies.enabled === true) {
-            return (
-                <Dropdown
-                    text="Roles and Policies"
-                    pointing
-                    className="link item"
-                >
-                    <Dropdown.Menu>
-                        <Dropdown.Item
-                            as={NavLink}
-                            to="/ui/catalog"
-                        >
-                            Catalog
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                            Policies
-                        </Dropdown.Item>
-                        <Dropdown.Item
-                            as={NavLink}
-                            to="/ui/selfservice"
-                        >
-                            Self Service Permissions
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                            API Health
-                        </Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            );
-        }
-        return null;
+  const getAvatarImage = () => {
+    if (userInfo.employee_photo_url) {
+      return (
+        <Image
+          alt={userInfo.user}
+          avatar
+          src={userInfo.employee_photo_url}
+          title={userInfo.user}
+        />
+      );
     }
+    return null;
+  };
 
-    const generateAdvancedDropDown = () => {
-        if (userInfo.pages.config.enabled === true) {
-            return (
-                <Dropdown
-                    text="Advanced"
-                    pointing
-                    className="link item"
-                >
-                    <Dropdown.Menu>
-                        <Dropdown.Item>
-                            Audit
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                            Config
-                        </Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            );
-        }
-        return null;
-    }
-
-    const getAvatarImage = () => {
-        if (userInfo.employee_photo_url) {
-            return (
-                <Image
-                    alt={userInfo.user}
-                    avatar
-                    src={userInfo.employee_photo_url}
-                    title={userInfo.user}
-                />
-            );
-        }
-        return null;
-    };
-
-
-    return (
-        <Menu
-            color="red"
-            fixed="top"
-            inverted
-            style={{
-                height: "72px",
-                marginBottom: "0"
-            }}
-        >
-            <Menu.Item
-                as="a"
-                header
-                name="header"
-                style={{
-                    fontSize: "20px",
-                    textTransform: "uppercase",
-                    width: "240px",
-                }}
-                href="/ui/"
-            >
-                <Image
-                    size='mini'
-                    src='/static2/images/logo192.png'
-                    style={{ marginRight: '1.5em' }}
-                />
-                ConsoleMe
-            </Menu.Item>
-            <Menu.Menu position="left">
-                <Menu.Item
-                    active={false}
-                    exact
-                    as={NavLink}
-                    name={"roles"}
-                    to="/ui/"
-                >
-                    AWS Console Roles
-                </Menu.Item>
-                {generateGroupsDropDown()}
-                {generatePoliciesDropDown()}
-                {generateAdvancedDropDown()}
-            </Menu.Menu>
-            <Menu.Menu position="right">
-                <Menu.Item>
-                    {getAvatarImage()}
-                </Menu.Item>
-            </Menu.Menu>
-        </Menu>
-    );
+  return (
+    <Menu
+      color="red"
+      fixed="top"
+      inverted
+      style={{
+        height: "72px",
+        marginBottom: "0",
+      }}
+    >
+      <Menu.Item
+        as="a"
+        header
+        name="header"
+        style={{
+          fontSize: "20px",
+          textTransform: "uppercase",
+          width: "240px",
+        }}
+        href="/ui/"
+      >
+        <Image
+          size="mini"
+          src="/static2/images/logo192.png"
+          style={{ marginRight: "1.5em" }}
+        />
+        ConsoleMe
+      </Menu.Item>
+      <Menu.Menu position="left">
+        <Menu.Item active={false} exact as={NavLink} name={"roles"} to="/ui/">
+          AWS Console Roles
+        </Menu.Item>
+        {generatePoliciesDropDown()}
+        {generateAdvancedDropDown()}
+      </Menu.Menu>
+      <Menu.Menu position="right">
+        <Menu.Item>{getAvatarImage()}</Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
 };
 
 export default ConsoleMeHeader;
