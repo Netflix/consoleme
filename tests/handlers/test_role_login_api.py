affdd5239c5f8aa0fffacb25c1829eeb3d2240b8
Curtis Castrapel <ccastrapel@netflix.com>
2020-11-09 15:00:57 -0800

    Change status code of multiple role response to 300

--- tests/handlers/test_role_login_api.py
+++ tests/handlers/test_role_login_api.py
@@ -40,7 +40,7 @@ class TestRoleLoginApi(AsyncHTTPTestCase):
         }
 
         response = self.fetch("/api/v2/role_login/role", headers=headers)
-        self.assertEqual(response.code, 403)
+        self.assertEqual(response.code, 300)
         response_j = json.loads(response.body)
         self.assertEqual(
             response_j["message"],
