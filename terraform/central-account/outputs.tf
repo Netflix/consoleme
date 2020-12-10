a38b0b8426a6d7057e4b847593ea339c0db732ba
Yoni Leitersdorf <y@indeni.com>
2020-12-10 12:32:26 -0800

    Updated output to point to the load balancer, as well as refer to Systems Manager for console access.

--- terraform/central-account/outputs.tf
+++ terraform/central-account/outputs.tf
@@ -1,6 +1,6 @@
-output "public_ip" {
-  description = "The public IP address of the EC2 instance running the application."
-  value       = module.server.public_ip
+output "public_fqdn" {
+  description = "The public address of the EC2 instance running the application."
+  value       = aws_lb.public-to-private-lb.dns_name
 }
 
 output "zREADME" {
@@ -13,16 +13,9 @@ If you're following the Terraform demo environment and your Terraform apply was
 successful, you can wait for a few minutes and then visit the ConsoleMe dashboard
 at the following URL:
 
-$ http://${module.server.public_ip[0]}:8081
+$ http://${aws_lb.public-to-private-lb.dns_name}:8081
 
-If you want to access the server, make sure you add your private key to your
-SSH agent:
-
-$ ssh-add path/to/private_key.pem
-
-Then SSH into the server:
-
-$ ssh -A ec2-user@${module.server.public_ip[0]}
+If you want to access the server over SSH, consider using Systems Manager or a VPN in.
 
 If you want to experiment with the ConsoleMe config, you can find the config files at the path:
 
