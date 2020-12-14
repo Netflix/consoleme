3dbffc211218f35401951b86d30d21c60c782878
Curtis Castrapel <ccastrapel@netflix.com>
2020-12-14 12:22:43 -0800

    Upgrade pre-commot dependencies

--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -5,11 +5,13 @@ module.exports = function override(config, env) {
   const appTarget = process.env.APP_TARGET || "default";
   config.plugins = [
     ...config.plugins,
-    new webpack.NormalModuleReplacementPlugin(/(.*)Default(\.*)/, function (
-      resource
-    ) {
-      resource.request = appTarget === "default" ? resource.request : appTarget;
-    }),
+    new webpack.NormalModuleReplacementPlugin(
+      /(.*)Default(\.*)/,
+      function (resource) {
+        resource.request =
+          appTarget === "default" ? resource.request : appTarget;
+      }
+    ),
     new MonacoWebpackPlugin({
       languages: ["yaml", "json"],
     }),
