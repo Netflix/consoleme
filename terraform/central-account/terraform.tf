175cc5802e7afe9d965e32654272a0f4d679d94e
Morgan MacKechnie <morgan@mackechnie.uk>
2021-04-27 17:08:11 +0100

    terraform template_file is deprecated in 0.12. Use templatefile() instead. With a recent aws provider version, this has the added benefit of not redeploying the instance every time (#9081)
    Co-authored-by: Morgan MacKechnie <morgan.mackechnie@netprotect.com>

--- terraform/central-account/terraform.tf
+++ terraform/central-account/terraform.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = var.region
 }
 
-provider "template" {
-}
-
 terraform {
+  required_providers {
+    aws = {
+      version = ">= 3"
+    }
+  }
   required_version = ">= 0.13.0"
   // Unfortunately, we can't use variables to specify a backend S3 bucket / DynamoDB table for Terraform state
   // consistency. If you want to make use of this, you'll need to make a copy of these terraform files in your own
