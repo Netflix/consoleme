095f94d2d5247f11c08b5d62a131e9c4dd665c7c
Curtis Castrapel <ccastrapel@netflix.com>
2021-05-25 07:11:07 -0700

    Clean up cloudwatch metric handler

--- default_plugins/consoleme_default_plugins/plugins/metrics/cloudwatch/__init__.py
+++ default_plugins/consoleme_default_plugins/plugins/metrics/cloudwatch/__init__.py
@@ -1,30 +1,53 @@
-import threading
+import concurrent.futures
+import sys
 from typing import Dict, Optional, Union
 
 import boto3
+import sentry_sdk
 
 from consoleme.config import config
 from consoleme_default_plugins.plugins.metrics.base_metric import Metric
 
 cloudwatch = boto3.client("cloudwatch", region_name=config.region)
+log = config.get_logger()
+
+
+def log_metric_error(future):
+    try:
+        future.result()
+    except Exception as e:
+        log.error(
+            {
+                "function": f"{__name__}.{sys._getframe().f_code.co_name}",
+                "message": "Error sending metric",
+                "error": str(e),
+            },
+            exc_info=True,
+        )
+        sentry_sdk.capture_exception()
 
 
 class CloudWatchMetric(Metric):
     def __init__(self):
         self.namespace = config.get("metrics.cloudwatch.namespace", "ConsoleMe")
+        self.executor = concurrent.futures.ThreadPoolExecutor(
+            config.get("metrics.cloudwatch.max_threads", 10)
+        )
 
     def send_cloudwatch_metric(self, metric_name, dimensions, unit, value):
-        cloudwatch.put_metric_data(
-            Namespace=self.namespace,
-            MetricData=[
+        kwargs = {
+            "Namespace": self.namespace,
+            "MetricData": [
                 {
                     "MetricName": metric_name,
                     "Dimensions": dimensions,
                     "Unit": unit,
-                    "Value": value,
+                    "Value": "dsafasdf",
                 },
             ],
-        )
+        }
+        future = self.executor.submit(cloudwatch.put_metric_data, **kwargs)
+        future.add_done_callback(log_metric_error)
 
     def generate_dimensions(self, tags):
         dimensions = []
@@ -37,18 +60,12 @@ class CloudWatchMetric(Metric):
     def count(self, metric_name, tags=None):
         dimensions = self.generate_dimensions(tags)
 
-        threading.Timer(
-            0, self.send_cloudwatch_metric, (metric_name, dimensions, "Count", 1)
-        ).start()
+        self.send_cloudwatch_metric(metric_name, dimensions, "Count", 1)
 
     def gauge(self, metric_name, metric_value, tags=None):
         dimensions = self.generate_dimensions(tags)
 
-        threading.Timer(
-            0,
-            self.send_cloudwatch_metric,
-            (metric_name, dimensions, "Count", metric_value),
-        ).start()
+        self.send_cloudwatch_metric(metric_name, dimensions, "Count", metric_value)
 
     def timer(
         self,
@@ -57,6 +74,4 @@ class CloudWatchMetric(Metric):
     ) -> None:
         dimensions = self.generate_dimensions(tags)
 
-        threading.Timer(
-            0, self.send_cloudwatch_metric, (metric_name, dimensions, "Count/Second", 1)
-        ).start()
+        self.send_cloudwatch_metric(metric_name, dimensions, "Count/Second", 1)
