b45f0cd43935672f149dfd5cc779a23825570cd3
jaydhulia <jaydhulia@hotmail.com>
2021-05-30 17:00:13 -0600

    YAML generation of consoleme style config

--- ui/src/components/generate_config/GenerateConfig.js
+++ ui/src/components/generate_config/GenerateConfig.js
@@ -3,6 +3,7 @@ import * as Survey from "survey-react";
 import "survey-react/survey.css";
 import { questions_json } from "./questions.js";
 import MonacoEditor from "react-monaco-editor";
+import yaml from "js-yaml";
 
 const generated_config_editor_options = {
   selectOnLineNumbers: true,
@@ -19,8 +20,25 @@ function GenerateConfig() {
   const [results, setResults] = useState({});
   const onComplete = (results) => {
     setComplete(true);
-    // TODO actual generation of config based on results here
-    setResults(results.data);
+    // TODO some manipulation for certain keys (lists, list of dicts)
+    let resultsConsoleMeStyle = {};
+    for (const [key, value] of Object.entries(results.data)) {
+      if (!key.startsWith("__")) {
+        updateNestedObj(resultsConsoleMeStyle, key, value);
+      }
+    }
+    setResults(resultsConsoleMeStyle);
+  };
+  const updateNestedObj = (d, k, v) => {
+    if (k.includes(".")) {
+      const dotIndex = k.indexOf(".");
+      const splits = [k.slice(0, dotIndex), k.slice(dotIndex + 1)];
+      const nextObj = d.hasOwnProperty(splits[0]) ? d[splits[0]] : {};
+      d[splits[0]] = updateNestedObj(nextObj, splits[1], v);
+    } else {
+      d[k] = v;
+    }
+    return d;
   };
   const surveyContent = () => {
     if (!complete) {
@@ -32,14 +50,14 @@ function GenerateConfig() {
         />
       );
     }
-    // Placeholder for now, the results need to be converted to YAML that ConsoleMe configs are in
+    // TODO: show copy/save to file button
     return (
       <MonacoEditor
-        height="500px"
-        language="json"
+        height="700px"
+        language="yaml"
         width="100%"
         theme="vs-dark"
-        value={JSON.stringify(results, null, 4)}
+        value={yaml.dump(results, 4)}
         options={generated_config_editor_options}
         textAlign="center"
       />
