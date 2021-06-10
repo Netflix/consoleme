c0fbd16b44c9867da2002f6c6c23c67924319c25
ConsoleMe <ccastrapel@netflix.com>
2021-06-09 21:22:41 -0700

    Allow specifying an S3 CONFIG_LOCATION

--- default_plugins/consoleme_default_plugins/plugins/config/config.py
+++ default_plugins/consoleme_default_plugins/plugins/config/config.py
@@ -3,7 +3,12 @@ import os
 import urllib.parse
 from typing import List
 
-from consoleme.lib.aws import split_s3_path
+
+def split_s3_path(s3_path):
+    path_parts = s3_path.replace("s3://", "").split("/")
+    b = path_parts.pop(0)
+    k = "/".join(path_parts)
+    return b, k
 
 
 class Config:
