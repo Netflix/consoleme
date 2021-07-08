fb63dbbd13bd306c00e173a709ed123f8821fa82
avishayil <avishay.il@gmail.com>
2021-07-08 08:46:09 +0300

    Linter stuff

--- cdk/cdk/consoleme_ecs_service/consoleme_spoke_accounts_stack.py
+++ cdk/cdk/consoleme_ecs_service/consoleme_spoke_accounts_stack.py
@@ -4,7 +4,7 @@ Spoke accounts stack for running ConsoleMe on ECS
 
 from aws_cdk import aws_iam as iam
 from aws_cdk import core as cdk
-from constants import SPOKE_BASE_NAME, MAIN_ACCOUNT_ID
+from constants import MAIN_ACCOUNT_ID, SPOKE_BASE_NAME
 
 
 class ConsolemeSpokeAccountsStack(cdk.Stack):
