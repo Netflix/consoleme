c8eeffb94a7e0b64f452731365f52fd60d09440f
Patrick Sanders <psanders@netflix.com>
2021-09-01 15:53:14 -0700

    Ensure all roles are returned by /api/v2/audit/roles

--- consoleme/models.py
+++ consoleme/models.py
@@ -504,6 +504,10 @@ class WebResponse(BaseModel):
     status_code: Optional[int] = None
     message: Optional[str] = None
     errors: Optional[List[str]] = None
+    count: Optional[int] = None
+    total: Optional[int] = None
+    page: Optional[int] = None
+    last_page: Optional[int] = None
     data: Optional[Union[Dict[str, Any], List]] = None
 
 
