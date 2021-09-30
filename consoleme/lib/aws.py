aa27fc7672c5d8f59856577d5b5f342110c00775
Curtis Castrapel <ccastrapel@gmail.com>
2021-09-29 21:03:35 -0700

    Compile regex

--- consoleme/lib/aws.py
+++ consoleme/lib/aws.py
@@ -2049,13 +2049,13 @@ def sanitize_session_name(unsanitized_session_name):
     """
     import re
 
-    valid_characters_re = r"[\w+=,.@-]"
+    valid_characters_re = re.compile(r"[\w+=,.@-]")
 
     sanitized_session_name = ""
     max_length = 64  # Session names have a length limit of 64 characters
     for char in unsanitized_session_name:
         if len(sanitized_session_name) == max_length:
             break
-        if re.match(valid_characters_re, char):
+        if valid_characters_re.match(char):
             sanitized_session_name += char
     return sanitized_session_name
