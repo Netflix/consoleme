e7315c6e083a14872a00c8239b15f2fd8e2e4646
Alex Bainbridge <abainbridge@netflix.com>
2021-11-10 16:45:42 -0800

    broke out slack notification into helper function

--- consoleme/lib/slack.py
+++ consoleme/lib/slack.py
@@ -1,4 +1,5 @@
 import sys
+import uuid
 
 import tornado.escape
 import ujson as json
@@ -14,19 +15,19 @@ log = config.get_logger()
 stats = get_plugin_by_name(config.get("plugins.metrics", "default_metrics"))()
 
 
-async def send_slack_notification_new_request(
-    extended_request: ExtendedRequestModel, admin_approved, approval_probe_approved
+async def send_slack_notification_new_policy_request(
+        extended_request: ExtendedRequestModel, admin_approved, approval_probe_approved
 ):
     """
     Sends a notification using specified webhook URL about a new request created
     """
-    if not config.get("slack.notifications_enabled", False):
-        return
 
     function = f"{__name__}.{sys._getframe().f_code.co_name}"
     requester = extended_request.requester_email
     arn = extended_request.principal.principal_arn
     stats.count(function, tags={"user": requester, "arn": arn})
+
+    payload_id = uuid.uuid4()
 
     log_data: dict = {
         "function": f"{__name__}.{sys._getframe().f_code.co_name}",
@@ -36,40 +37,23 @@ async def send_slack_notification_new_request(
         "request": extended_request.dict(),
         "admin_approved": admin_approved,
         "approval_probe_approved": approval_probe_approved,
+        "payload_id": payload_id
     }
     log.debug(log_data)
-    slack_webhook_url = config.get("slack.webhook_url")
-    if not slack_webhook_url:
-        log_data["message"] = "Missing webhook URL for slack notification"
-        log.error(log_data)
-        return
+
     payload = await get_payload(
         extended_request, requester, arn, admin_approved, approval_probe_approved
     )
-    http_headers = HTTPHeaders({"Content-Type": "application/json"})
-    http_req = HTTPRequest(
-        url=slack_webhook_url,
-        method="POST",
-        headers=http_headers,
-        body=json.dumps(payload),
-    )
-    http_client = AsyncHTTPClient(force_instance=True)
-    try:
-        await http_client.fetch(request=http_req)
-        log_data["message"] = "Slack notification sent"
-        log.debug(log_data)
-    except (ConnectionError, HTTPClientError) as e:
-        log_data["message"] = "Error occurred sending slack notification"
-        log_data["error"] = str(e)
-        log.error(log_data)
+
+    return await send_slack_notification(payload, payload_id)
 
 
 async def get_payload(
-    extended_request: ExtendedRequestModel,
-    requester: str,
-    arn: str,
-    admin_approved: bool,
-    approval_probe_approved: bool,
+        extended_request: ExtendedRequestModel,
+        requester: str,
+        arn: str,
+        admin_approved: bool,
+        approval_probe_approved: bool,
 ):
     request_uri = await get_policy_request_uri_v2(extended_request)
     pre_text = "A new request has been created"
@@ -117,3 +101,35 @@ async def get_payload(
         ]
     }
     return payload
+
+
+async def send_slack_notification(payload, payload_id):
+    """
+    Sends a notification using specified webhook URL about a new request created
+    """
+    if not config.get("slack.notifications_enabled", False):
+        log.info(f"Slack notifications not enabled. Not sending payload: {payload_id}")
+        return
+
+    slack_webhook_url = config.get("slack.webhook_url")
+    if not slack_webhook_url:
+        log.error(f"Missing webhook URL for slack notification. Not sending payload: {payload_id}")
+        return
+
+    http_headers = HTTPHeaders({"Content-Type": "application/json"})
+    http_req = HTTPRequest(
+        url=slack_webhook_url,
+        method="POST",
+        headers=http_headers,
+        body=json.dumps(payload),
+    )
+
+    http_client = AsyncHTTPClient(force_instance=True)
+    try:
+        await http_client.fetch(request=http_req)
+        log_data["message"] = "Slack notification sent"
+        log.debug(log_data)
+    except (ConnectionError, HTTPClientError) as e:
+        log_data["message"] = "Error occurred sending slack notification"
+        log_data["error"] = str(e)
+        log.error(log_data)
